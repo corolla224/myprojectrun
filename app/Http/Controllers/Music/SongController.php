a9815968a993ebadc1d3c16c358149748f7c30d2
corolla224 <corolla224@gmail.com>
2017-06-01 17:12:59 +0700

    Add files via upload

--- app/Http/Controllers/Music/SongController.php
+++ app/Http/Controllers/Music/SongController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Music;
 
+use DB;
 use App\Http\Controllers\Controller;
 
 /**
@@ -40,5 +41,31 @@ class SongController extends Controller
       'album' => 'Morning4'
     );
     return view('song/play', $this->data);
+
+
+  }
+
+  public function band()
+  {
+      $aCss = array('css/song/style.css');
+      $aScript = array('js/song/main.js');
+      /*$band = DB::table('blog_tbl')->get();
+      var_dump($band);
+      die();*/
+      //$band = DB::table('blog_tbl')->find('3');
+      //$band = DB::select('select title,blog_th from blog_tbl where deleted = ?', [0]);
+      //$band = DB::insert('insert into blog_tbl (title, blog_th) values (?, ?)', ['green day', 'best of rock']);
+      //$band_up =DB::update('update blog_tbl set blog_th = "Best of rock band" where title = ?', ['green day']);
+      //$band_del = DB::delete('delete from blog_tbl where title = ?', ['green day']);
+      //$band = DB::table('blog_tbl')->get();
+      //dd($band);
+      
+
+      $this->data = array(
+      'band' => 'Metallica',
+      'song' => 'BLACK'
+      //'style' => $aCss
+    );
+    return view('song/band', $this->data);
   }
 }
